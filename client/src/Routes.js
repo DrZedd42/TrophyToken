c6c1859680f951e98e637d5f670105a72ef87567
Will Shahda <will.shahda@gmail.com>
2019-02-02 14:05:28 -0500

    Create Routes component

---
+++ client/src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './About';
+import CreateTrophy from './CreateTrophy';
+import ViewAccountTrophies from './ViewAccountTrophies';
+import ViewTrophy from './ViewTrophy';
+
+export default props => {
+  return (
+    <>
+      <Route exact path="/" component={About} />
+      <Route path="/create" component={CreateTrophy} />
+      <Route path="/view/:address" render={props => (
+        <ViewAccountTrophies
+          {...props}
+          key={props.match.params.address}
+        />
+      )} />
+      <Route path="/trophy/:tokenId" component={ViewTrophy} />
+    </>
+  );
+};
