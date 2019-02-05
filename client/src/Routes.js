ff4c965035909154c53e6820967bf6db0188166e
Will Shahda <will.shahda@gmail.com>
2019-02-05 13:38:55 -0500

    Create Terms and Conditions page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import About from './About';
+import Terms from './Terms';
 import CreateTrophy from './CreateTrophy';
 import ViewAccountTrophies from './ViewAccountTrophies';
 import ViewTrophy from './ViewTrophy';
@@ -9,6 +10,7 @@ export default props => {
   return (
     <>
       <Route exact path="/" component={About} />
+      <Route path="/terms" component={Terms} />
       <Route path="/create" component={CreateTrophy} />
       <Route path="/view/:address" render={props => (
         <ViewAccountTrophies
