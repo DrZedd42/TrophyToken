0fb484851859331eaa35c3ee4444b39f00aa9996
Will Shahda <will.shahda@gmail.com>
2019-02-05 11:46:51 -0500

    Update About page styling

--- client/src/About.js
+++ client/src/About.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { toClass } from 'recompose';
 import {
   Button,
   Card,
+  Container,
   Divider,
-  Grid,
   Header,
   Image,
   Item,
-  List,
   Segment
 } from 'semantic-ui-react';
 import Layout from './Layout';
@@ -16,7 +17,7 @@ import './About.css';
 
 import trophy from './trophies/trophy_8.svg';
 import trophy_10 from './trophies/trophy_10.svg';
-import trophy_14 from './trophies/trophy_14.svg';
+import trophy_12 from './trophies/trophy_12.svg';
 import trophy_16 from './trophies/trophy_16.svg';
 import ethereumLogo from './ETHEREUM-ICON_Black_small.png';
 
@@ -24,100 +25,106 @@ export default () => {
   return (
     <Layout>
       <div className="About">
-        <Segment padded size="large">
-          <Grid centered>
-            <Grid.Row>
-              <Grid.Column width={3} >
-                <Image src={trophy} size="small" floated="right" />
-              </Grid.Column>
-              <Grid.Column width={11} >
-                <Header as="h1" className="BigHeader">
-                  TrophyToken
-                  <Header.Subheader>
-                    Create custom digital trophies with Ethereum.
-                  </Header.Subheader>
-                </Header>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={15} textAlign="center">
-                <Button size="big" primary>Create New Trophy</Button>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+        <Segment
+          inverted
+          vertical
+          padded="very"
+          size="large"
+          textAlign="center"
+        >
+          <Container>
+            <Image src={trophy} size="small" centered />
+            <Header as="h1" className="BigHeader">
+              TrophyToken
+              <Header.Subheader>
+                Create custom digital trophies with Ethereum.
+              </Header.Subheader>
+            </Header>
+            <Divider hidden />
+            <Button size="big" primary as={toClass(NavLink)} to="/create">
+              Create New Trophy
+            </Button>
+          </Container>
         </Segment>
 
         <Divider hidden />
 
-        <Header as="h1" textAlign="center">
-          Send custom trophies to your friends
-          <Header.Subheader>
-            Write your own inscription and select from a variety of trophy types.
-          </Header.Subheader>
-        </Header>
-        <Card.Group centered itemsPerRow={3}>
-          <Card className="trophyCard" fluid>
-            <Card.Content>
-              <Image
-                src={trophy_16}
-                fluid
-                centered
-                className="trophyCardImage"
-              />
-            </Card.Content>
-            <Card.Content>
-              <Card.Header textAlign="center">The Top 10 Whale Award</Card.Header>
-            </Card.Content>
-          </Card>
-          <Card className="trophyCard" fluid>
-            <Card.Content>
-              <Image
-                src={trophy_14}
-                fluid
-                centered
-                className="trophyCardImage"
-              />
-            </Card.Content>
-            <Card.Content>
-              <Card.Header textAlign="center">Medal For Shitcoin Trading Bravery</Card.Header>
-            </Card.Content>
-          </Card>
-          <Card className="trophyCard" fluid>
-            <Card.Content>
-              <Image
-                src={trophy_10}
-                fluid
-                centered
-                className="trophyCardImage"
-              />
-            </Card.Content>
-            <Card.Content>
-              <Card.Header textAlign="center">The 2018 Crash HODL Trophy</Card.Header>
-            </Card.Content>
-          </Card>
-        </Card.Group>
+        <Container>
+          <Header as="h1" textAlign="center">
+            Send custom trophies to your friends
+            <Header.Subheader>
+              Write your own inscription and select from a variety of trophy types.
+            </Header.Subheader>
+          </Header>
 
-        <Divider hidden section />
+          <Divider hidden />
 
-        <Grid centered>
-          <Grid.Row>
-            <Grid.Column width={9}>
-              <Item.Group>
-                <Item>
-                  <Item.Image src={ethereumLogo} size="mini" />
-                  <Item.Content verticalAlign="middle">
-                    <Item.Header>ERC-721 Token</Item.Header>
-                    <Item.Description>
-                      Each trophy is a unique ERC-721 non-fungible token on the Ethereum blockchain.
-                    </Item.Description>
-                  </Item.Content>
-                </Item>
-              </Item.Group>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+          <Card.Group centered itemsPerRow={4}>
+            <Card className="trophyCard" fluid>
+              <Card.Content>
+                <Image
+                  src={trophy_16}
+                  fluid
+                  centered
+                  className="trophyCardImage"
+                />
+              </Card.Content>
+              <Card.Content>
+                <Card.Header textAlign="center">
+                  The Top 10 Whale Award
+                </Card.Header>
+              </Card.Content>
+            </Card>
+            <Card className="trophyCard" fluid>
+              <Card.Content>
+                <Image
+                  src={trophy_12}
+                  fluid
+                  centered
+                  className="trophyCardImage"
+                />
+              </Card.Content>
+              <Card.Content>
+                <Card.Header textAlign="center">
+                  Master Shitcoin Trader Shield
+                </Card.Header>
+              </Card.Content>
+            </Card>
+            <Card className="trophyCard" fluid>
+              <Card.Content>
+                <Image
+                  src={trophy_10}
+                  fluid
+                  centered
+                  className="trophyCardImage"
+                />
+              </Card.Content>
+              <Card.Content>
+                <Card.Header textAlign="center">
+                  The 2018 Crash HODL Trophy
+                </Card.Header>
+              </Card.Content>
+            </Card>
+          </Card.Group>
 
-        <Divider hidden section />
+          <Divider hidden section />
+
+          <Container text>
+            <Item.Group>
+              <Item>
+                <Item.Image src={ethereumLogo} size="mini" />
+                <Item.Content verticalAlign="middle">
+                  <Item.Header>ERC-721 Token</Item.Header>
+                  <Item.Description>
+                    Each trophy is a unique ERC-721 non-fungible token on the Ethereum blockchain.
+                  </Item.Description>
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Container>
+
+          <Divider hidden section />
+        </Container>
       </div>
     </Layout>
   );
